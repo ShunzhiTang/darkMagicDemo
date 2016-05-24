884523835b3686d162d9c813604d5e00bb1bc232
tang <tangdream@163.com>
2016-05-24 11:17:07 +0800

    creat project

---
+++ darkMagicDemo/darkMagicDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  darkMagicDemo
+//
+//  Created by tang on 16/5/24.
+//  Copyright © 2016年 shunzhitang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
