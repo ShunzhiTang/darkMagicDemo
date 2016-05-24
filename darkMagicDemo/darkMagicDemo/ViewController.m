d9001bb5dce4454e8875fe382107ee48c8e7d7ab
tang <tangdream@163.com>
2016-05-24 14:57:49 +0800

    几个简单的dark magic 的关键字

--- darkMagicDemo/darkMagicDemo/ViewController.m
+++ darkMagicDemo/darkMagicDemo/ViewController.m
@@ -8,20 +8,75 @@
 
 #import "ViewController.h"
 
+
+
+#define onExit\
+    __strong void(^block)(void) __attribute__((cleanup(blockCleanUp),unused)) = ^
+
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+/**
+ 
+   1、 __attribute__((cleanup(...))) ,用于修饰一个变量 ，在它作用域结束时可以自动执行一个指定的方法
+ 所谓的作用域结束包括： 包括大括号结束、return、goto、break、exception等各种情况。
+ 当然，可以修饰的变量不止NSString，自定义Class或基本类型都是可以的：
+ 
+  2、可以使用 __attribute__((cleanup(...)))修饰一个block的方法
+ 
+ */
+
+static void stringCleanUp(__strong NSString **string){
+    
+    NSLog(@"%@" , *string);
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    __strong NSString *string __attribute__((cleanup(stringCleanUp))) = @"nihaoya";
+    
+    [self testBlockAttribute];
+    
+    NSLog(@"-------------");
 }
+
+#pragma mark: __attribute__((cleanup(...))) 修饰一个block
+
+static void blockCleanUp(__strong  void(^*block)(void)){
+    
+    (*block)();
+    
+}
+
+- (void)testBlockAttribute{
+    
+    
+    // 加了个`unused`的attribute用来消除`unused variable`的warning
+    
+//    __strong void(^block)(void) __attribute__((cleanup(blockCleanUp), unused)) = ^{
+//        NSLog(@"I'm dying...");
+//    };
+    
+//    __strong void(^block)(void) __attribute__((cleanup(blockCleanUp) , unused)) = ^(void){
+//        
+//        NSLog(@"wohahaha");
+//    };
+    
+    onExit{
+        
+        NSLog(@"GO Go Go");
+    };
+    
+}
+
+
+
 
 @end
--- darkMagicDemo/darkMagicDemo/main.m
+++ darkMagicDemo/darkMagicDemo/main.m
@@ -7,10 +7,40 @@
 //
 
 #import <UIKit/UIKit.h>
+
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
+    
+    NSLog(@"++++");
+    
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
+
+
+/**
+     构造器 ：Constructor  ， 析构器：Destructor
+    
+    这两个属性的函数会在可执行文件（shared library）load和unload 时被调用 ， 可以理解为在main（）函数的前和return后执行
+ */
+
+__attribute__((constructor))
+
+static void beforeMain(void){
+    
+    NSLog(@"beforeMain");
+}
+
+
+__attribute__((destructor))
+
+static void afterMain(void){
+    
+    NSLog(@"after");
+    
+}
+
+
+
